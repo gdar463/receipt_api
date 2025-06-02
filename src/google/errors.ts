5ed27b1f78ed277a49c371790295039d73e04626
gdar463 <gdar463@gmail.com>
2025-06-02 22:02:04 +0200

    fixed jwenotfounderror typo

--- src/google/errors.ts
+++ src/google/errors.ts
@@ -1,6 +1,6 @@
 export class JWENotFoundError extends Error {
   constructor() {
     super("The JWE was not found in the db");
-    this.name = "JWENotFound";
+    this.name = "JWENotFoundError";
   }
 }
