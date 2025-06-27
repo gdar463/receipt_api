7058eb0b68f425b2751160ecaa63b778df898016
gdar463 <gdar463@gmail.com>
2025-06-27 15:59:53 +0200

    db overhaul

--- src/item/routes/get.ts
+++ src/item/routes/get.ts
@@ -2,25 +2,16 @@ import db from "@/db";
 import { receipts } from "@/db/schema";
 import { and, eq } from "drizzle-orm";
 import { ReceiptNotFoundError } from "@/item/errors";
-import { getFileByID } from "@/google/drive";
-import { createComponentMap } from "../components/utils";
 import type { ReceiptDB } from "../types";
 
 export async function getReceipt(userId: string, receiptId: string) {
   const rows = await db
-    .select({ driveId: receipts.driveId })
+    .select()
     .from(receipts)
     .where(and(eq(receipts.userId, userId), eq(receipts.id, receiptId)));
   if (rows.length == 0) {
     throw new ReceiptNotFoundError();
   }
-  const receiptDB = await getFileByID(rows[0].driveId, userId);
-  const receipt = {
-    ...receiptDB,
-    componentMap: createComponentMap(receiptDB.components),
-  } as ReceiptDB;
-  return {
-    id: receiptId,
-    data: receipt,
-  };
+  const receipt = rows[0] as Omit<ReceiptDB, "componentMap">;
+  return receipt;
 }
