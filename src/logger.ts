cfcfb5b0d4efd6ed21b1d5aaa43e33bdf4a33867
gdar463 <gdar463@gmail.com>
2025-06-27 14:36:43 +0200

    added console only log option

--- src/logger.ts
+++ src/logger.ts
@@ -5,24 +5,29 @@ const axiom = new Axiom({
   token: process.env.AXIOM_TOKEN!,
 });
 
-const axiomLogger = process.env.AXIOM_LOG_CONSOLE
-  ? new Logger({
-      transports: [
-        new AxiomJSTransport({
-          axiom,
-          dataset: process.env.AXIOM_DATASET!,
-        }),
-        new ConsoleTransport({ prettyPrint: true }),
-      ],
-    })
-  : new Logger({
-      transports: [
-        new AxiomJSTransport({
-          axiom,
-          dataset: process.env.AXIOM_DATASET!,
-        }),
-      ],
-    });
+const consoleVar = process.env.AXIOM_LOG_CONSOLE;
+
+const axiomLogger =
+  consoleVar === "1"
+    ? new Logger({
+        transports: [
+          new AxiomJSTransport({
+            axiom,
+            dataset: process.env.AXIOM_DATASET!,
+          }),
+          new ConsoleTransport({ prettyPrint: true }),
+        ],
+      })
+    : consoleVar === "2"
+    ? new Logger({ transports: [new ConsoleTransport({ prettyPrint: true })] })
+    : new Logger({
+        transports: [
+          new AxiomJSTransport({
+            axiom,
+            dataset: process.env.AXIOM_DATASET!,
+          }),
+        ],
+      });
 
 export const logger = {
   info: (message: string, meta?: object) => {
