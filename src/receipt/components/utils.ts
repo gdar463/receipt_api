411576f5beaf77d70142674966ee732c660a194f
gdar463 <gdar463@gmail.com>
2025-06-27 23:21:54 +0200

    fixed map type

--- src/receipt/components/utils.ts
+++ src/receipt/components/utils.ts
@@ -2,7 +2,7 @@ import type { ComponentType, ReceiptComponent } from "./types";
 
 export function createComponentMap(
   components: ReceiptComponent[]
-): Partial<Record<ComponentType, ReceiptComponent & { index: number }>> {
+): Partial<{ [K in ComponentType]: ReceiptComponent<K> & { index: number } }> {
   return Object.fromEntries(
     components.map((c, i) => [c.type, { ...c, index: i }])
   );
